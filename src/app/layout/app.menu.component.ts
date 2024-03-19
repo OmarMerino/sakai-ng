157f6fde7cf8133b4c078e35ffe937b4495c0831
OmarMerino <102824045+OmarMerino@users.noreply.github.com>
2024-03-18 22:28:12 -0300

    adaptacion.

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -17,13 +17,15 @@ export class AppMenuComponent implements OnInit {
             {
                 label: 'Home',
                 items: [
-                    { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
+                    { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/admin'] }
                 ]
             },
-            {
+            {   /*esta parte es el*/
                 label: 'UI Components',
                 items: [
-                    { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout'] },
+                    { label: 'Productos', icon: 'pi pi-fw pi-box', routerLink: ['/uikit/formlayout'] },
+                    { label: 'ss', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout'] },
+                    /**/
                     { label: 'Input', icon: 'pi pi-fw pi-check-square', routerLink: ['/uikit/input'] },
                     { label: 'Float Label', icon: 'pi pi-fw pi-bookmark', routerLink: ['/uikit/floatlabel'] },
                     { label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
